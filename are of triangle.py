c8e0eb08309f04fe6a42886d76602f642abb6b9f
GeorgeJogic <georgejogic@gmail.com>
2025-01-08 22:39:48 +0530

    Add files via upload
    it is a simple program

---
+++ are of triangle.py
@@ -0,0 +1,10 @@
+x= float(input("Enter lenght of the base of the triangle to find its AREA(in centimeter)(aviod alphabets to prevent error): "))
+y= float(input("Enter lenght of the height from base to top of the triangle (in centimeter)(aviod alphabets to prevent error): "))
+AREA= 0.5*x*y
+if type(x,y,AREA)== float:
+    print(AREA)
+else:
+    print("ig")
+
+
+   
